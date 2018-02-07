bfd719353f743c603fc9008c6b9caf1fa2cb775e
Andres Vallone <andresvallone@MacBook-Pro-de-Andres.local>
2018-02-07 11:11:52 +0100

    Fixing arguemnts definition

--- R/msp.R
+++ R/msp.R
@@ -19,33 +19,11 @@
 
  
 msp <- function(x,y,r=0.5,type = "ask",...){
-	if(is.character(x)!=TRUE) stop(" You must provide a character")
-	clean.y <- y
-	unique.y <- unique(y)
-	search <- agrep(x,unique.y, max.distance = r,value=TRUE)
-	if(length(search)==0) stop("No mactching, nothing to replace")
-	if (length(search)==1) {
-		 type.first(search)
-	} else if (type =="ask"){ 
-		 type.ask(search) 
-	} else if (type=="first"){ 
-		 type.first(search) 
-	} else  { 
-		 type.long(search) 
-		}
-	print (ls())
-	if (exists(add)==TRUE){
-		replace<-add
-	} else {
-		replace <- search[as.numeric(goodone)]
-	}
-	for ( j in change){
-		clean.y[which(y==search[change[j]])] <- replace
-	}
-	return(clean.y)
-
-
-	type.ask<-function(search){
+		#checks
+		if(is.character(x)!=TRUE) stop(" You must provide a character")
+		
+		#type definitions	
+		type.ask<-function(search){
 			display<-c(search, "None")
 			cat(display, fill=max(nchar(search)) ,labels= paste0("[",seq_along(display),"]"))
 			goodone <<- readline("Please select the correct option to replace: ")
@@ -62,26 +40,40 @@ msp <- function(x,y,r=0.5,type = "ask",...){
 			} else {
 				change <<- seq_along(search)
 			}
-	}
+		}
 
-	type.first<-function(search){
-			goodone <- 1
+		type.first<-function(search){
+			goodone <<- 1
 			change <<- seq_along(search)
-	}
+		}
 
-	type.long<-function(search){
+		type.long<-function(search){
 			long <- sapply(search,nchar)
 			goodone <<- which(long==max(long))[1]
-			change <- seq_along(search)	
-	}
-}
-
-z<-c()
-
-
-
-z1<-z
-for(i in seq_along(z)){
-	print(i)
-	z1<-msp(z1[i],z1,type="first")
+			change <<- seq_along(search)	
+		}	
+	
+		#global function
+		clean.y <- y
+		unique.y <- unique(y)
+		search <- agrep(x,unique.y, max.distance = r,value=TRUE)
+		if(length(search)==0) stop("No mactching, nothing to replace")
+		if (length(search)==1) {
+			 type.first(search)
+		} else if (type =="ask"){ 
+			 type.ask(search) 
+		} else if (type=="first"){ 
+			 type.first(search) 
+		} else  { 
+			 type.long(search) 
+		}
+		if (exists("add")==TRUE){
+			replace<-add
+		} else {
+			replace <- search[as.numeric(goodone)]
+		}
+		for ( j in change){
+			clean.y[which(y==search[change[j]])] <- replace
+		}
+	return(clean.y)
 }
